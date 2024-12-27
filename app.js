34ba5b5ee74d16440db68d1a6771c1138efbe705
PedroSamuelBittencourt <bittencourtpedro2003@gmail.com>
2024-12-27 14:49:03 -0300

    New Desing
    This commit was responsible for creating a new design for this web page.

--- app.js
+++ app.js
@@ -6,27 +6,36 @@ var classificacao = " "
 var num = 0
 function imc(){
     
-
+    
     var num1 = peso.value
     var num2 = altura.value
-    peso.value = " "
-    altura.value = " "
-    num = num1 / (num2 * num2)
-
-    if(num<18.5){
-       classificacao = ", você está abaixo do peso ideal."
-    }else if(num<24.9){
-        classificacao = ", você está com o peso ideal."
-    }else if(num<29.9){
-        classificacao = ", você está com Sobepeso."
-    }else if(num<34.9){
-        classificacao = ", você está com Obesidade grau 1."
-    }else if(num<39.9){
-        classificacao = ", você está com Obesidade grau 2."
-    }else if(num>=40){
-        classificacao = ", você está com Obesidade grau 3."
+    // Verifica se os campos estão vazios ou se não são números válidos
+    if (isNaN(num1) || isNaN(num2) || num1 <= 0 || num2 <= 0) {
+        return;
     }
-    document.getElementById('imc').innerHTML = "Seu imc é: "+ num.toFixed(2) + classificacao 
+
+
+
+    num = num1 / (num2 * num2)
+    
+        if(num<18.5){
+            classificacao = ", você está abaixo do peso ideal."
+         }else if(num<24.9){
+             classificacao = ", você está com o peso ideal."
+         }else if(num<29.9){
+             classificacao = ", você está com Sobepeso."
+         }else if(num<34.9){
+             classificacao = ", você está com Obesidade grau 1."
+         }else if(num<39.9){
+             classificacao = ", você está com Obesidade grau 2."
+         }else if(num>=40){
+             classificacao = ", você está com Obesidade grau 3."
+         }
+         document.getElementById('imc').innerHTML = "Seu imc é: "+ num.toFixed(2) + classificacao 
+    
+   
+         peso.value = " "
+         altura.value = " "
 }
 document.getElementById('imc').innerHTML = "Seu imc é: "+ num + classificacao 
 btn.addEventListener('click', imc)
